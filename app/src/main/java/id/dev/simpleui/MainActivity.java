c61d4f1452711eb0a3126a6ca6123a99157f1a9b
Yudis <yudistirosaputro@gmail.com>
2018-03-11 02:45:47 +0700

    Initial commit

---
+++ app/src/main/java/id/dev/simpleui/MainActivity.java
@@ -0,0 +1,25 @@
+package id.dev.simpleui;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button buttonLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        buttonLogin = findViewById(R.id.btnLogin);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Home.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
