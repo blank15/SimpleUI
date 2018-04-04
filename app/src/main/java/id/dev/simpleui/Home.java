d77bc0f68b0b347466e12d0b5b96c7cb68d8a4e5
blank15 <yudistiro6@gmail.com>
2018-04-04 12:14:00 +0700

    new login

--- app/src/main/java/id/dev/simpleui/Home.java
+++ app/src/main/java/id/dev/simpleui/Home.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Home extends AppCompatActivity implements View.OnClickListener {
 
@@ -12,29 +13,33 @@ public class Home extends AppCompatActivity implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Button buttonRelative = findViewById(R.id.btnRelative);
-        Button buttonLinear = findViewById(R.id.btnLinear);
-        Button buttonConstrain   = findViewById(R.id.btnConstrain);
-        buttonRelative.setOnClickListener(this);
-        buttonLinear.setOnClickListener(this);
-        buttonConstrain.setOnClickListener(this);
+//        Button buttonRelative = findViewById(R.id.btnRelative);
+//        Button buttonLinear = findViewById(R.id.btnLinear);
+//        Button buttonConstrain   = findViewById(R.id.btnConstrain);
+//        buttonRelative.setOnClickListener(this);
+//        buttonLinear.setOnClickListener(this);
+//        buttonConstrain.setOnClickListener(this);
+        TextView textViewNama = findViewById(R.id.textNamaTampil);
+        TextView textViewNIM = findViewById(R.id.textNimTampil);
+        textViewNama.setText(getIntent().getStringExtra("nama"));
+        textViewNIM.setText(getIntent().getStringExtra("nim"));
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.btnRelative:
-                Intent intent = new Intent(this,MainActivity.class);
-                startActivity(intent);
-                break;
-            case R.id.btnLinear:
-                Intent intent2 = new Intent(this,Main2Activity.class);
-                startActivity(intent2);
-                break;
-            case R.id.btnConstrain:
-                Intent intent3 = new Intent(this,Main3Activity.class);
-                startActivity(intent3);
-                break;
+//            case R.id.btnRelative:
+//                Intent intent = new Intent(this,MainActivity.class);
+//                startActivity(intent);
+//                break;
+//            case R.id.btnLinear:
+//                Intent intent2 = new Intent(this,Main2Activity.class);
+//                startActivity(intent2);
+//                break;
+//            case R.id.btnConstrain:
+//                Intent intent3 = new Intent(this,Main3Activity.class);
+//                startActivity(intent3);
+//                break;
         }
     }
 }
--- app/src/main/java/id/dev/simpleui/MainActivity.java
+++ app/src/main/java/id/dev/simpleui/MainActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button buttonLogin;
@@ -13,12 +16,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         buttonLogin = findViewById(R.id.btnLogin);
-
+        final EditText textViewNama = findViewById(R.id.ediTextUsername);
+        final EditText textViewPss = findViewById(R.id.ediTextPss);
+        final EditText textViewNIM = findViewById(R.id.textNim);
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,Home.class);
-                startActivity(intent);
+                if(textViewNama.getText().toString().isEmpty() || textViewPss.getText().toString().isEmpty()){
+                    Toast.makeText(MainActivity.this, "Password atau username kosong", Toast.LENGTH_SHORT).show();
+                }else {
+                    if (textViewNama.getText().toString().equals("Yudistiro") && textViewPss.getText().toString().equals("12345678")) {
+                        Intent intent = new Intent(MainActivity.this,Home.class);
+                        intent.putExtra("nama",textViewNama.getText().toString());
+                        intent.putExtra("nim",textViewNIM.getText().toString());
+                        startActivity(intent);
+                    }else {
+                        Toast.makeText(MainActivity.this, "Password atau username salah", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+
             }
         });
     }
